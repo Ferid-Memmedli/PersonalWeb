8316cfec62f13ccdd06ab58683d5107ee05ea166
Ferid Memmedli <shahasist@gmail.com>
2021-12-08 23:06:16 +0400

    Edit

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -11,10 +11,25 @@ namespace PersonalWeb.Controllers
 {
   public class HomeController : Controller
   {
+    DbPersonalWebEntities context;
+    public HomeController()
+    {
+      context = new DbPersonalWebEntities();
+    }
     public ActionResult Index()
     {
       SettingWeb settingWeb = JsonFileManager.JsonConvertToObject<SettingWeb>("App_Data\\SettingWeb.json");
       return View(settingWeb);
+    }
+
+    public PartialViewResult BlogPartial()
+    {
+      return PartialView(context.Blogs.ToList());
+    }
+
+    public PartialViewResult PortfolioPartial()
+    {
+      return PartialView(context.Portfolios.ToList());
     }
   }
 }
